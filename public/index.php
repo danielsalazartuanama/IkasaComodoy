5351dcb33b37f2e3342a73780d311d1d53bb51ad
isrrael12 <isrrael120320@gmail.com>
2021-05-19 18:00:02 -0500

    Implementando plantilla informativa

--- public/index.php
+++ public/index.php
@@ -2,6 +2,12 @@
 //los require once porque queremos cargar en memoria y este duisponible para todos los controladores y se habra 
 
 use Libs\Core;
+use Dotenv\Dotenv;
+
 require_once '../vendor/autoload.php';
 require_once '../config/config.php';
-$core=new Core();
+echo realpath(__DIR__ . '/../');
+echo MAINPATH;
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+$core = new Core();
