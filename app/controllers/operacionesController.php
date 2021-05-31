c07ba7537bc32018e1cdcdf9972a8ad9f1627357
jhonydaniel <jhonydanielsalazar53@gmail.com>
2021-05-31 17:48:13 -0500

    Vistas marca Y conexion a la bd

---
+++ app/controllers/operacionesController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controllers;
+use Libs\Controller;
+class OperacionesController extends Controller {
+    
+   
+    public function index()
+    {
+        echo "Bienvenidos";
+    }
+
+    public function suma(int $n1, int $n2): int{
+        return $n1+$n2;
+    }
+
+}
+
+
